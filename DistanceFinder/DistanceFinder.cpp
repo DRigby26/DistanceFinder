230dda148ecace69f060cd171c13c466e780c8b3
David <drigby6@gmail.com>
2018-03-09 12:37:28 -0800

    Submitted version
    Finds closest house using min reduction

--- DistanceFinder/DistanceFinder.cpp
+++ DistanceFinder/DistanceFinder.cpp
@@ -24,26 +24,36 @@ struct House {
 	int dist;
 } house;
 
-
-bool compareDistance(const House &a, const House &b)
-{
-	return a.dist < b.dist;
-}
-
 // Calculates the nearest house
-void closestHouse(std::vector<House> &house)
+int closestHouse(std::vector<House> &house)
 {
-	
-		std::sort(house.begin(), house.end(), compareDistance);
-		 // for (int i = 0; i < house.size(); i++)
-		 // {
-			 // std::cout << "\n Distance from house " << house[i].ID << " to station is " << house[i].dist << std::endl;
-		 // }
+	int shared_min = std::numeric_limits<int>::max();
 
-		std::cout << "The closest house is house " << house[0].ID << " and its coordinates are " << "X:" << house[0].x << " Y:" << house[0].y <<  std::endl;
+#pragma omp parallel
+	{
+		int min = std::numeric_limits<int>::max();
+#pragma omp for nowait
+		for (int i = 0; i < house.size(); i++)
+		{
+			min = std::min(house[i].dist, min);
+			
+		}
 
+#pragma omp critical 
+		{
+			shared_min = std::min(shared_min, min);
+
+		}
+		
+		
+	}
 	
+	
+	return shared_min;
+
 }
+
+
 
 // Calculate distance between each house and the station using sqrt(a^2 + b^2) = c
 int distanceCalculator(int x1, int x2, int y1, int y2)
@@ -60,6 +70,7 @@ int distanceCalculator(int x1, int x2, int y1, int y2)
 
 }
 
+
 House addHouse(int x, int y, int ID, int dist)
 {
 	House house = { x, y, ID, dist }; 
@@ -69,42 +80,60 @@ House addHouse(int x, int y, int ID, int dist)
 int main()
 { 
 	int n,x,y;
-	// int i;
+	
 	srand(time(0));
 	std::vector<House> H;
 	Station S[1];
-	std::cout << "Give Railroad station coordinates. ";
+	std::cout << "Give Railroad station coordinates. " << std::endl;
+	std::cout << "X: ";
 	std::cin >> S[0].x;
+	std::cout << "Y: ";
 	std::cin >> S[0].y;
-	std::cout << "How many houses? ";
-	std::cin >> n; 
 
-	for (int i = 0; i < n; i++)
-	{
-		x = rand() % 100;
-		y = rand() % 100;
-		H.push_back(addHouse(x, y, i, 0));
-	}
+		std::cout << "How many houses? " << std::endl;
+		std::cin >> n;
 
-#pragma omp parallel 
-	 {
-		 int p, threadID;
-		 p = omp_get_num_threads();
-		 // threadID = omp_get_thread_num();
-		
-		 // if (threadID == 0) numOfthreads = p;
- #pragma omp for schedule(static, n/p)
 		for (int i = 0; i < n; i++)
 		{
-			// std::cout << "\n I am thread number " << omp_get_thread_num() << std::endl;
-			// std::cout << "\n Total threads " << p << std::endl;
-			H[i].dist = distanceCalculator(H[i].x, S[0].x, H[i].y, S[0].y);
-			
+			x = rand() % 10000;
+			y = rand() % 10000;
+			H.push_back(addHouse(x, y, i, 0));
 		}
 
-	 }
+#pragma omp parallel
+		{
+			int p;
+			p = omp_get_num_threads();
+
+#pragma omp for schedule(static, n/p) 
+			for (int i = 0; i < n; i++)
+			{
+
+				H[i].dist = distanceCalculator(H[i].x, S[0].x, H[i].y, S[0].y);
+
+			}
+
+
+		}
+		
+
+		// Will take forever to print out if you are calculating millions of houses 
+		// but you can use it to see the program work for smaller numbers of n
+		/*
+		for (int i = 0; i < n; i++)
+		{
+		std::cout << "House " << H[i].ID << " is a distance of " << H[i].dist << std::endl;
+		}
+		*/
+		
+		
+
+		std::cout << "The closest house is " << closestHouse(H) << " Miles away " << std::endl;
+
+		 
+
 	
-	closestHouse(H);
+	
 
     return 0;
 }
